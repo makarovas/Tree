9a003d1def3410e8e3825d6b5e5ee3e42e4b49fc
alexander_makarov <alexander.makarov@tabby.ai>
2021-11-23 22:12:50 +0200

    refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,17 @@ import Layout from "./Story/Layout";
 import DeviceTree from "./pages/device-tree";
 import store from "./store/store";
 
-const App = () => {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Layout>
-          <Switch>
-            <Redirect from="/" exact to="/main" />
-            <Route path="/main" exact component={DeviceTree} />
-          </Switch>
-        </Layout>
-      </BrowserRouter>
-    </Provider>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Redirect from="/" exact to="/main" />
+          <Route path="/main" exact component={DeviceTree} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
--- client/src/components/Tabs.js
+++ client/src/components/Tabs.js
@@ -3,30 +3,28 @@ import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 
-const PaperTabs = ({ tabValue, handleTabChange }) => {
-  return (
-    <div>
-      <Paper
-        variant="outlined"
-        elevation={12}
-        style={{
-          padding: 8,
-          border: "1px solid darkblue",
-        }}
+const PaperTabs = ({ tabValue, handleTabChange }) => (
+  <div>
+    <Paper
+      variant="outlined"
+      elevation={12}
+      style={{
+        padding: 8,
+        border: "1px solid darkblue",
+      }}
+    >
+      <Tabs
+        value={tabValue}
+        indicatorColor="secondary"
+        textColor="secondary"
+        onChange={handleTabChange}
+        aria-label="disabled tabs example"
       >
-        <Tabs
-          value={tabValue}
-          indicatorColor="secondary"
-          textColor="secondary"
-          onChange={handleTabChange}
-          aria-label="disabled tabs example"
-        >
-          <Tab label="Tree View" />
-          <Tab label="Grouping by type" />
-        </Tabs>
-      </Paper>
-    </div>
-  );
-};
+        <Tab label="Tree View" />
+        <Tab label="Grouping by type" />
+      </Tabs>
+    </Paper>
+  </div>
+);
 
 export default PaperTabs;
